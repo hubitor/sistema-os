ae0a34ef0e8993a4dbf9d61020eff9b2ecec068d
Eder Rogério <ederrdls@hotmail.com>
2018-10-25 15:22:34 -0300

    iniciar estrutura com servidor e escrita de dados .csv

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require ('express');
+const fs = require ('fs');
+const bodyParser = require ('body-parser');
+
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use('/assets', express.static('assets'));
+app.use(bodyParser.urlencoded());
+
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+
+app.post('/', function(req, res){
+    console.log(req.body);
+    let os = req.body.os;
+    let conserto = req.body.conserto;
+    let data = req.body.data;
+    
+    let dados = [os, conserto, data];
+    
+    
+    fs.writeFile('dados.csv', dados, function(erro){
+        if (erro){
+            console.log(erro);
+            return;
+        }
+    });
+    console.log("Arquivo salvo com sucesso");
+    res.render('home');
+    
+});
+
+app.listen(3000, ()=>{
+    console.log ("Servidor iniciado em localhost://3000");
+});
