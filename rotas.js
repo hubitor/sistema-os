8f86273a665e8943563b78d61c869b5779e06f34
Eder Rogério <ederrdls@hotmail.com>
2019-02-21 15:06:07 -0300

    conectar com o banco da empresa e integrar ao sistema

--- rotas.js
+++ rotas.js
@@ -6,6 +6,21 @@ const pesquisar     = require ('./pesquisar');
 // const exibirEstoque     = require ('./exibirEstoque');
 const ObjectID      = require ('mongodb').ObjectID;
 
+const connStr = "Server=xxx.xxx.xxx.xxx;Database=xxx;User Id=xxx;Password=xxx;";
+const sql = require("mssql");
+
+
+sql.connect(connStr)
+.then(conn => global.conn = conn)
+.catch(err => console.log(err));
+
+function execSQLQuery(sqlQry, res){
+    global.conn.request()
+               .query(sqlQry)
+               .then(result => res.send("funcionou"))
+               .catch(err => res.json(err));
+}
+
 const router        = express.Router();
 
 //Funções GET **************************************************************************************
@@ -38,7 +53,14 @@ router.get('/login', function(req, res){
 });
 
 router.get('/home', function(req, res) {
-    let nome = req.cookies
+    let nome = "ssdsad";
+    nome = req.cookies;
+    //caso não esteja logado, ir para a tela de login
+    if(nome.nome === undefined){
+        res.redirect('login');
+        return;
+    }
+
     pesquisar(req, res, null, nome);
 });
 
@@ -133,10 +155,12 @@ router.post('/home', function(req, res){
     let entrada     = CorreioRecepcao;
     let conserto    = req.body.conserto.toUpperCase();
     let data        = req.body.data.split('-');
+    let dataDelta   = (`${data[0]}-${data[1]}-${data[2]}`);
     let databr      = (`${data[2]}/${data[1]}/${data[0]}`);
 
     if(os === 0 || entrada === null || conserto === "" || data[2] === undefined){
         res.send('Erro: preencha todos os dados');
+        return;
     }
     
     req.db.collection('dados').insert({
@@ -147,7 +171,11 @@ router.post('/home', function(req, res){
         conserto: conserto, 
         data:     databr
     });    
+
+    // execSQLQuery(`UPDATE Clientes SET Nome='${nome}', CPF='${cpf}' WHERE ID=${id}`, res);
     
+    execSQLQuery(`UPDATE ITEMAT SET desc_problema2='LAB: ${conserto}', posicao = 'CONSERTADO', tecnico = '${nome}', dt_conserto = '${dataDelta}' WHERE cod_assistencia= ${os}`, res);
+    console.log(dataDelta);
     res.redirect('home');      
 });
 
